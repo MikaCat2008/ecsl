5e9268dce623a5a4a8367d19977cd2b9e4241944
MikaCat2008 <dimalogat@gmail.com>
2024-12-03 16:27:46 +0200

    init

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from .builder import Builder as Builder
+from .tokenizer import Token as Token, Tokenizer as Tokenizer, TokenType as TokenType
+from .syntax_tree import Node as Node, StateType as StateType, SyntaxTree as SyntaxTree
+
+from .tools import get_properties as get_properties
+
+__version__ = 1, 0
